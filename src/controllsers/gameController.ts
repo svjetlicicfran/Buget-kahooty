44e052e9e62260036a7c5245599616d5ca2d7d8e
Fran Svjetlicic <svjetlicicfran@gmail.com>
2026-03-15 18:41:06 +0100

    Added isOnline to Player type and createSesstion in GameController

--- src/controllsers/gameController.ts
+++ src/controllsers/gameController.ts
@@ -1,10 +1,16 @@
 import { GameSession } from "../models/GameSession";
+import { Question } from "../models/Question";
+import { Player } from "../models/Player";
+import { GameState } from "../models/GameState";
+import { Answer } from "../models/Answer";
 
 export class GameController{
     private sessions: Map<string, GameSession> = new Map();
 
-    
-
-
+    createSession(pin: string, hostSocketId: string, questions: Question[]): GameSession{
+        const session = new GameSession(pin, hostSocketId, questions);
+        this.sessions.set(pin, session);
+        return session;
+    }
 
 }
--- src/models/Player.ts
+++ src/models/Player.ts
@@ -1,6 +1,7 @@
 export interface Player {
-    playerId: string;
-    socketId: string;
+    playerId: string; // The permanent ID (UUID or similar)
+    socketId: string; // The current temporary connection ID
     name: string;
     score: number;
+    isOnline: boolean;
 }
