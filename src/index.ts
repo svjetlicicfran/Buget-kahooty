a8d6fcfddcfdd80e95a95192339eeba1b2e951d9
Fran Svjetlicic <svjetlicicfran@gmail.com>
2026-03-16 13:16:43 +0100

    Created main funciton

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import loginRouter from "./routes/login";
+import registerRouter from "./routes/register";
+
+import http from "http";
+import express from "express";
+import cors from "cors";
+
+const PORT: number = 8000;
+
+async function main() {
+    const app: express.Application = express();
+    const server: http.Server = http.createServer(app);
+    
+    app.use(express.json());
+    app.use(cors());
+    app.use('/auth', loginRouter);
+    app.use('/auth', registerRouter);
+
+    //Join connect socket.io sever later
+
+    server.listen(PORT, () =>{
+        console.log(`erver runing at http://localhost:${PORT}`);
+    });
+
+}
+
+main().catch((err) => {
+    console.log(err);
+});
